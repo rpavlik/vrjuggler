ee90f921f1f027709e1423b02fc63026acf8cc2d
allenb <allenb@08b38cba-cd3b-11de-854e-f91c5b6e4272>
2006-04-10 16:38:17 +0000

    Restructure branches.
    git-svn-id: http://vrjuggler.googlecode.com/svn/juggler/branches/2.0@18475 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/md/NSPR/Thread/ThreadKeyNSPR.h
@@ -0,0 +1,173 @@
+/****************** <VPR heading BEGIN do not edit this line> *****************
+ *
+ * VR Juggler Portable Runtime
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Patrick Hartling, Kevin Meinert, Carolina Cruz-Neira
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ****************** <VPR heading END do not edit this line> ******************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2005 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+/**
+ * \file
+ *
+ * @note This file MUST be included by vprThread.h, not the other way around.
+ */
+
+#ifndef _VPR_THREAD_KEY_POSIX_H_
+#define _VPR_THREAD_KEY_POSIX_H_
+
+#include <vpr/vprConfig.h>
+#include <prthread.h>
+
+
+namespace vpr
+{
+
+// Key destructor function type.
+typedef PRThreadPrivateDTOR KeyDestructor;
+
+/** \class ThreadKeyNSPR ThreadKeyNSPR.h vpr/Thread/Thread.h
+ *
+ * Wrapper around NSPR thread-specific data.  It is typedef'd to
+ * vpr::ThreadKey.
+ */
+class ThreadKeyNSPR
+{
+public:
+   /**
+    * Default constructor.  After calling this, one of the keycreate()
+    * overloads must be called to finish the key allocation process.
+    *
+    * @see keycreate
+    */
+   ThreadKeyNSPR(KeyDestructor destructor = NULL)
+   {
+      keycreate(destructor);
+   }
+
+   /**
+    * Releases this key.
+    */
+   ~ThreadKeyNSPR()
+   {
+      keyfree();
+   }
+
+   /**
+    * Allocates a key that is used to identify data that is specific to each
+    * thread in the process, is global to all threads in the process, and is
+    * is destroyed using the spcefied destructor function.
+    *
+    * @post A key is created and is associated with the specified destructor
+    *       function and argument.
+    *
+    * @param destructor The destructor function for the key.
+    *
+    * @return 0 is returned upon successful completion.
+    * @return -1 is returned if an error occurs.
+    */
+   int keycreate(KeyDestructor destructor = NULL)
+   {
+      PRStatus ret = PR_NewThreadPrivateIndex(&mKeyID,
+                                              (KeyDestructor) destructor);
+      return (PR_SUCCESS == ret ? 0 : -1);
+   }
+
+   /**
+    * Frees up this key so that other threads may reuse it.
+    *
+    * @pre This key must have been properly created using the keycreate()
+    *      member function.
+    * @post This key is destroyed using the destructor function previously
+    *       associated with it, and its resources are freed.
+    *
+    * @return 0 is returned upon successful completion.
+    * @return -1 is returned if an error occurs.
+    */
+   int keyfree()
+   {
+      // Key auto-matically freed
+      setspecific(NULL);
+      return 0;
+   }
+
+   /**
+    * Binds value to the thread-specific data key for the calling thread.
+    *
+    * @pre The specified key must have been properly created using the
+    *      keycreate() member function.
+    * @post The specified value is associated with the key for the calling
+    *       thread.
+    *
+    * @param value Address containing data to be associated with the
+    *              specified key for the current thread.
+    *
+    * @return 0 is returned upon successful completion.
+    * @return -1 is returned if an error occurs.
+    */
+   int setspecific(void* value)
+   {
+      PRStatus ret = PR_SetThreadPrivate(mKeyID, value);
+      return (PR_SUCCESS == ret ? 0 : -1);
+   }
+
+   /**
+    * Stores the current value bound to \c mKeyID for the calling thread into
+    * the location pointed to by \p valuep.
+    *
+    * @pre The specified key must have been properly created using the
+    *      keycreate() member function.
+    * @post The value associated with the key is obtained and stored in the
+    *       pointer valuep so that the caller may work with it.
+    *
+    * @param valuep Address of the current data value associated with the key.
+    *
+    * @return 0 is returned upon successful completion.
+    * @return -1 is returned if an error occurs.
+    */
+   int getspecific(void** valuep)
+   {
+      *valuep = PR_GetThreadPrivate(mKeyID);
+      return 0;
+   }
+
+private:
+   PRUintn mKeyID;        /**< Thread key ID */
+};
+
+} // End of vpr namespace
+
+
+#endif  /* _VPR_THREAD_KEY_NSPR_H_ */
---
+++ modules/vapor/vpr/md/POSIX/IO/Socket/InetAddrBSD.cpp
@@ -0,0 +1,431 @@
+/****************** <VPR heading BEGIN do not edit this line> *****************
+ *
+ * VR Juggler Portable Runtime
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Patrick Hartling, Kevin Meinert, Carolina Cruz-Neira
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ****************** <VPR heading END do not edit this line> ******************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2005 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include <vpr/vprConfig.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/param.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+
+#ifdef HAVE_STRINGS_H
+#include <strings.h>
+#endif
+
+#include <boost/concept_check.hpp>
+
+#include <vpr/md/POSIX/IO/Socket/InetAddrBSD.h>
+#include <vpr/Util/Debug.h>
+
+#ifndef INADDR_NONE
+#  define INADDR_NONE 0xffffffff   /* -1 return */
+#endif
+
+
+namespace vpr
+{
+
+vpr::ReturnStatus getIfAddrs(std::vector<vpr::InetAddrBSD>& hostAddrs,
+                             const bool withLoopback);
+
+const InetAddrBSD InetAddrBSD::AnyAddr;      // Default constructor defaults to ANY addr
+
+InetAddrBSD::InetAddrBSD()
+{
+   memset(&mAddr, 0, sizeof(mAddr));
+   setAddressValue(INADDR_ANY);
+   setPort(0);
+   setFamily(SocketTypes::INET);
+}
+
+vpr::ReturnStatus InetAddrBSD::getLocalHost(vpr::InetAddrBSD& hostAddr)
+{
+   char local_host_name[MAXHOSTNAMELEN + 1];
+   vpr::ReturnStatus status;
+
+   bzero(local_host_name, sizeof(local_host_name));
+
+   if ( gethostname(local_host_name, MAXHOSTNAMELEN) == 0 )
+   {
+      hostAddr.setAddress(std::string(local_host_name), 0);
+   }
+   else
+   {
+      status.setCode(vpr::ReturnStatus::Fail);
+   }
+
+   return status;
+}
+
+vpr::ReturnStatus
+InetAddrBSD::getAllLocalAddrs(std::vector<vpr::InetAddrBSD>& hostAddrs,
+                              const bool withLoopback)
+{
+   return vpr::getIfAddrs(hostAddrs, withLoopback);
+}
+
+/**
+ * Constructs an address object using the given address.  It must be of the
+ * form <address>:<port> where <address> can be a hostname or a
+ * dotted-decimal IP address.
+ */
+vpr::ReturnStatus InetAddrBSD::setAddress(const std::string& address)
+{
+   std::string::size_type pos;
+   std::string host_addr, host_port;
+   vpr::Uint16 port;
+   vpr::ReturnStatus retval;
+
+   // Extract the address and the port number from the given string.
+   pos       = address.find(":");
+   host_addr = address.substr(0, pos);
+   host_port = address.substr(pos + 1);
+   port      = (Uint16) atoi(host_port.c_str());
+
+   retval = lookupAddress(host_addr);
+   setPort(port);
+   setFamily(vpr::SocketTypes::INET);
+
+   return retval;
+}
+
+vpr::ReturnStatus InetAddrBSD::setAddress(const std::string& address,
+                                          const Uint16 port)
+{
+   vpr::ReturnStatus retval;
+
+   retval = lookupAddress(address);
+   setPort(port);
+   setFamily(SocketTypes::INET);
+
+   return retval;
+}
+
+vpr::ReturnStatus InetAddrBSD::setAddress(const vpr::Uint32 address,
+                                          const vpr::Uint16 port)
+{
+   setAddressValue(address);
+   setPort(port);
+   setFamily(SocketTypes::INET);
+   return ReturnStatus();
+}
+
+unsigned char InetAddrBSD::getLength() const
+{
+#ifdef HAVE_SIN_LEN
+   return mAddr.sin_len;
+#else
+   return 0;
+#endif
+}
+
+void InetAddrBSD::setLength(const unsigned char length)
+{
+#ifdef HAVE_SIN_LEN
+   mAddr.sin_len = length;
+#else
+   boost::ignore_unused_variable_warning(length);
+#endif
+}
+
+/**
+ * Gets the protocol family of this address structure.
+ */
+vpr::SocketTypes::Domain InetAddrBSD::getFamily() const
+{
+   vpr::SocketTypes::Domain family;
+
+   switch ( mAddr.sin_family )
+   {
+#ifdef PF_LOCAL
+      case PF_LOCAL:
+#else
+      case PF_UNIX:
+#endif
+         family = SocketTypes::LOCAL;
+         break;
+      case PF_INET:
+         family = SocketTypes::INET;
+         break;
+#ifdef PF_INET6
+      case PF_INET6:
+         family = SocketTypes::INET6;
+         break;
+#endif
+#if defined(PF_LINK) || defined(PF_RAW)
+#ifdef PF_LINK
+      case PF_LINK:
+#else
+      case PF_RAW:
+#endif
+         family = SocketTypes::LINK;
+         break;
+#endif
+      default:
+         family = SocketTypes::INET;
+         break;
+   }
+
+   return family;
+}
+
+/**
+ * Sets the protocol family of this address structure.
+ */
+void InetAddrBSD::setFamily(const vpr::SocketTypes::Domain family)
+{
+   switch ( family )
+   {
+      case SocketTypes::LOCAL:
+#ifdef PF_LOCAL
+         mAddr.sin_family = PF_LOCAL;
+#else
+         mAddr.sin_family = PF_UNIX;
+#endif
+         break;
+      case SocketTypes::INET:
+         mAddr.sin_family = PF_INET;
+         break;
+      case SocketTypes::INET6:
+#ifdef PF_INET6
+         mAddr.sin_family = PF_INET6;
+#else
+         fprintf(stderr,
+                 "[vpr::InetAddrBSD] WARNING: IPv6 not supported on this host!\n");
+#endif
+         break;
+#if defined(PF_LINK) || defined(PF_RAW)
+      case SocketTypes::LINK:
+#ifdef PF_LINK
+         mAddr.sin_family = PF_LINK;
+#else
+         mAddr.sin_family = PF_RAW;
+#endif
+         break;
+#endif
+      default:
+         fprintf(stderr,
+                 "[vpr::InetAddrBSD] ERROR: Unknown socket family value %d\n",
+                 family);
+         break;
+   }
+}
+
+vpr::Uint16 InetAddrBSD::getPort() const
+{
+   return ntohs(mAddr.sin_port);
+}
+
+void InetAddrBSD::setPort(const vpr::Uint16 port)
+{
+   mAddr.sin_port = htons(port);
+}
+
+vpr::Uint32 InetAddrBSD::getAddressValue() const
+{
+   return ntohl(mAddr.sin_addr.s_addr);
+}
+
+/**
+ * Get the IP address associated with this structure as a human-readable
+ * string.
+ */
+std::string InetAddrBSD::getAddressString() const
+{
+   char ip_addr[sizeof("255.255.255.255")];
+   std::string ip_str;
+   union
+   {
+      char c[sizeof(vpr::Uint32)];
+      vpr::Uint32 value;
+   } addr;
+
+   addr.value = mAddr.sin_addr.s_addr;
+
+   snprintf(ip_addr, sizeof(ip_addr), "%u.%u.%u.%u", (Uint8) addr.c[0],
+            (Uint8) addr.c[1], (Uint8) addr.c[2], (Uint8) addr.c[3]);
+   ip_str = ip_addr;
+
+   return ip_str;
+}
+
+vpr::ReturnStatus InetAddrBSD::getHostname(std::string& hostname) const
+{
+   vpr::ReturnStatus status;
+
+   socklen_t salen;
+#if defined(HAVE_SIN_LEN)
+   salen = mAddr.sin_len;
+#else
+   salen = sizeof(mAddr);
+#endif
+
+   char addr[NI_MAXHOST];
+   memset((void*) &addr, 0, sizeof(addr));
+
+   int result = getnameinfo((sockaddr*) &mAddr, salen, addr, sizeof(addr),
+                            NULL, 0, NI_NAMEREQD);
+
+   if ( result == 0 )
+   {
+      hostname = addr;
+   }
+   else
+   {
+      status.setCode(vpr::ReturnStatus::Fail);
+      const int err_code(errno);
+      vprDEBUG(vprDBG_VPR, vprDBG_CRITICAL_LVL) 
+         << clrOutBOLD(clrRED, "ERROR")
+         << ": [vpr::InetAddrBSD::getHostname()] Could not find hostname for "
+         << inet_ntoa(mAddr.sin_addr) << ": " << strerror(err_code)
+         << std::endl << vprDEBUG_FLUSH;
+   }
+
+   return status;
+}
+
+std::vector<std::string> InetAddrBSD::getHostnames() const
+{
+   std::vector<std::string> names;
+   struct hostent* entry;
+
+   entry = gethostbyaddr((const char*) &mAddr.sin_addr,
+                         sizeof(mAddr.sin_addr), mAddr.sin_family);
+
+   if ( NULL != entry )
+   {
+      names.push_back(std::string(entry->h_name));
+
+      for ( char** ptr = entry->h_aliases; *ptr != NULL; ptr++ )
+      {
+         names.push_back(std::string(*ptr));
+      }
+   }
+
+   return names;
+}
+
+// Overloaded equality operator.
+bool InetAddrBSD::operator== (const InetAddrBSD& addr) const
+{
+   return (mAddr.sin_addr.s_addr == addr.mAddr.sin_addr.s_addr) &&
+          (mAddr.sin_port == addr.mAddr.sin_port) &&
+          (mAddr.sin_family == addr.mAddr.sin_family);
+}
+
+void InetAddrBSD::copyAddressValue(const char* addrValue)
+{
+   vprASSERT(addrValue != NULL);
+   memcpy((void*) &mAddr.sin_addr.s_addr, (void*) addrValue,
+          sizeof(mAddr.sin_addr.s_addr));
+}
+
+void InetAddrBSD::setAddressValue(const vpr::Uint32 addrValue)
+{
+   mAddr.sin_addr.s_addr = htonl(addrValue);
+}
+
+size_t InetAddrBSD::size() const
+{
+   return sizeof(mAddr);
+}
+
+size_t InetAddrBSD::addressSize() const
+{
+   return sizeof(mAddr.sin_addr.s_addr);
+}
+
+void InetAddrBSD::setSockaddr(const struct sockaddr* addr)
+{
+   memcpy((void*) &mAddr, (void*) addr, sizeof(mAddr));
+}
+
+struct sockaddr_in InetAddrBSD::toSockaddrInet()
+{
+   return mAddr;
+}
+
+void InetAddrBSD::copy(const InetAddrBSD& addr)
+{
+   memcpy((void*) &mAddr, (void*) &addr.mAddr, sizeof(mAddr));
+}
+
+/**
+ * Look up the given address and store the address in mAddr.
+ */
+vpr::ReturnStatus InetAddrBSD::lookupAddress(const std::string& address)
+{
+   vpr::ReturnStatus retval;
+   struct addrinfo* addrs;
+
+   struct addrinfo hints;
+   memset((void*) &hints, 0, sizeof(hints));
+   hints.ai_flags = AF_INET;
+
+   int result = getaddrinfo(address.c_str(), NULL, &hints, &addrs);
+
+   if ( result == 0 )
+   {
+      memcpy((void*) &mAddr, addrs->ai_addr, addrs->ai_addrlen);
+      freeaddrinfo(addrs);
+   }
+   else
+   {
+      vprDEBUG(vprDBG_ALL, vprDBG_WARNING_LVL)
+         << clrOutBOLD(clrYELLOW, "WARNING")
+         << ": Could not find address for '" << address << "': "
+         << gai_strerror(result) << std::endl << vprDEBUG_FLUSH;
+
+      retval.setCode(vpr::ReturnStatus::Fail);
+   }
+
+   return retval;
+}
+
+} // End of vpr namespace
