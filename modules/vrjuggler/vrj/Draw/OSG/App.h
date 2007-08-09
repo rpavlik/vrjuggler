rpavlik/vrjuggler

commit 602bac25d146b8cf2a8d38854ce2cbe412a7ea8e

Make the code more conformant with the coding standard and typical C++ usage guidelines. The following changes have been made:
   * Use unsigned int instead of just unsigned for clarity.
   * Use C++ casting instead of C-style casting.
   * Const-ify.

No functional changes.


git-svn-id: http://vrjuggler.googlecode.com/svn/juggler/trunk@20651 08b38cba-cd3b-11de-854e-f91c5b6e4272

modules/vrjuggler/vrj/Draw/OSG/App.h
@@ -440,10 +440,14 @@ inline void App::draw()
    user_data->getGlWindow()->getOriginSize(w_ox, w_oy, w_width, w_height);
 
    // compute unsigned versions of the viewport info (for passing to glViewport)
-   unsigned ll_x = unsigned(vp_ox*float(w_width));
-   unsigned ll_y = unsigned(vp_oy*float(w_height));
-   unsigned x_size = unsigned(vp_sx*float(w_width));
-   unsigned y_size = unsigned(vp_sy*float(w_height));
+   const unsigned int ll_x =
+      static_cast<unsigned int>(vp_o x * static_cast<float>(w_width));
+   const unsigned int ll_y =
+      static_cast<unsigned int>(vp_oy * static_cast<float>(w_height));
+   const unsigned int x_size =
+      static_cast<unsigned int>(vp_sx * static_cast<float>(w_width));
+   const unsigned int y_size =
+      static_cast<unsigned int>(vp_sy * static_cast<float>(w_height));
 
    //sv->setCalcNearFar(false);
    sv->setComputeNearFarMode(osgUtil::CullVisitor::DO_NOT_COMPUTE_NEAR_FAR);

commit 011cc5e19838b273cd59cf0aed83e72326cfa848

Fixed a typo that I introduced in r20651.
git-svn-id: http://vrjuggler.googlecode.com/svn/juggler/trunk@20662 08b38cba-cd3b-11de-854e-f91c5b6e4272

modules/vrjuggler/vrj/Draw/OSG/App.h
@@ -441,7 +441,7 @@ inline void App::draw()
 
    // compute unsigned versions of the viewport info (for passing to glViewport)
    const unsigned int ll_x =
-      static_cast<unsigned int>(vp_o x * static_cast<float>(w_width));
+      static_cast<unsigned int>(vp_ox * static_cast<float>(w_width));
    const unsigned int ll_y =
       static_cast<unsigned int>(vp_oy * static_cast<float>(w_height));
    const unsigned int x_size =