d2a1d586005b3b61477dd122d41a621afc94a2b0
Patrick Carlson <carlsonp@iastate.edu>
2013-02-01 15:22:41 -0600

    astyle

--- modules/gadgeteer/drivers/Open/VRPN/VRPNDevices.cpp
+++ modules/gadgeteer/drivers/Open/VRPN/VRPNDevices.cpp
@@ -12,23 +12,23 @@
 
 extern "C"
 {
-	GADGET_DRIVER_EXPORT(vpr::Uint32) getGadgeteerVersion()
-	{
-		return __GADGET_version;
-	}
+   GADGET_DRIVER_EXPORT( vpr::Uint32 ) getGadgeteerVersion()
+   {
+      return __GADGET_version;
+   }
 
-	GADGET_DRIVER_EXPORT(void) initDevice(gadget::InputManager* inputMgr)
-	{
-		vprDEBUG(gadgetDBG_INPUT_MGR, vprDBG_WARNING_LVL)
-			<< "Starting Gadget Driver Export" << std::endl
+   GADGET_DRIVER_EXPORT( void ) initDevice( gadget::InputManager* inputMgr )
+   {
+      vprDEBUG( gadgetDBG_INPUT_MGR, vprDBG_WARNING_LVL )
+            << "Starting Gadget Driver Export" << std::endl
             << vprDEBUG_FLUSH;
-		new gadget::DeviceConstructor<gadget::Vrpn>(inputMgr);
-		vprDEBUG(gadgetDBG_INPUT_MGR, vprDBG_WARNING_LVL)
-			<< "Middle." << std::endl
+      new gadget::DeviceConstructor<gadget::Vrpn>( inputMgr );
+      vprDEBUG( gadgetDBG_INPUT_MGR, vprDBG_WARNING_LVL )
+            << "Middle." << std::endl
             << vprDEBUG_FLUSH;
-		new gadget::DeviceConstructor<gadget::VRPNText>(inputMgr);
-		vprDEBUG(gadgetDBG_INPUT_MGR, vprDBG_WARNING_LVL)
-			<<  "Done." << std::endl
+      new gadget::DeviceConstructor<gadget::VRPNText>( inputMgr );
+      vprDEBUG( gadgetDBG_INPUT_MGR, vprDBG_WARNING_LVL )
+            <<  "Done." << std::endl
             << vprDEBUG_FLUSH;
-	}
+   }
 }
