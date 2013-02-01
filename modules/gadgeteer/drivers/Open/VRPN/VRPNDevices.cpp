d4c6369cb41d3f0dcaf9d6b57c97e7e0a669eebb
Patrick Carlson <carlsonp@iastate.edu>
2013-02-01 17:07:11 -0600

    delete old debug

--- modules/gadgeteer/drivers/Open/VRPN/VRPNDevices.cpp
+++ modules/gadgeteer/drivers/Open/VRPN/VRPNDevices.cpp
@@ -19,16 +19,7 @@ extern "C"
 
    GADGET_DRIVER_EXPORT(void) initDevice(gadget::InputManager* inputMgr)
    {
-      vprDEBUG(gadgetDBG_INPUT_MGR, vprDBG_WARNING_LVL)
-            << "Starting Gadget Driver Export" << std::endl
-            << vprDEBUG_FLUSH;
       new gadget::DeviceConstructor<gadget::Vrpn>(inputMgr);
-      vprDEBUG(gadgetDBG_INPUT_MGR, vprDBG_WARNING_LVL)
-            << "Middle." << std::endl
-            << vprDEBUG_FLUSH;
       new gadget::DeviceConstructor<gadget::VRPNText>(inputMgr);
-      vprDEBUG(gadgetDBG_INPUT_MGR, vprDBG_WARNING_LVL)
-            <<  "Done." << std::endl
-            << vprDEBUG_FLUSH;
    }
 }
